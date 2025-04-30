9052bbf01d7180c1ed8c84456ed4fabcd0e20b2a
app1 <saieesh.soma1919@gmail.com>
2025-04-30 14:23:04 +0530

    vercel check

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
-
 import { Layout } from '../components';
 import '../styles/globals.css';
 import { StateContext } from '../context/StateContext';
